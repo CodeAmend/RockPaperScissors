f55367d422ae370b344d7ad59ef12c92e44364b6
Michael Bruce Allen <CodeAmend@Gmail.Com>
2015-09-28 18:53:44 -0400

    cut fat out of code

--- test/com/rockpaperscissors/RPS.java
+++ test/com/rockpaperscissors/RPS.java
@@ -1,6 +1,5 @@
 package com.rockpaperscissors;
 
-import com.rockpaperscissors.Gesture;
 import org.junit.Assert;
 import org.junit.Test;
 import static com.rockpaperscissors.Gesture.ROCK;
@@ -10,7 +9,7 @@ import static com.rockpaperscissors.Gesture.SCISSORS;
 /**
  * Created by codeamend on 9/28/15.
  */
-public class RPS {
+public class RPS extends Assert {
 
     // I have three valid moves: Rock, Paper, or Scissors.
     // The result will be either
@@ -18,26 +17,20 @@ public class RPS {
     // or I will win, or the computer will win.
     // Scissors cut Paper, Paper covers Rock, Rock smashes Scissors.
 
-    // Winning
+
     @Test
     public void winningGesture() {
-        Assert.assertTrue("Winning", PAPER.beats(ROCK));
-        Assert.assertTrue("Winning", ROCK.beats(SCISSORS));
-        Assert.assertTrue("Winning", SCISSORS.beats(PAPER));
+        assertTrue(PAPER.beats(ROCK));
+        assertTrue(ROCK.beats(SCISSORS));
+        assertTrue(SCISSORS.beats(PAPER));
     }
 
-    //Losing
+
     @Test
     public void losingGesture() {
-        Assert.assertFalse("Losing", ROCK.beats(PAPER));
-        Assert.assertFalse("Losing", PAPER.beats(SCISSORS));
-        Assert.assertFalse("Losing", SCISSORS.beats(ROCK));
+        assertFalse(ROCK.beats(PAPER));
+        assertFalse(PAPER.beats(SCISSORS));
+        assertFalse(SCISSORS.beats(ROCK));
     }
 
-    @Test
-    public void tieGesture() {
-        //TODO: what do I want to happen here?
-        //Only two possibilities currently
-        //Perhaps a new enum Staus { WIN, LOSE, TIE }
-    }
 }
