d626276804954bfcea3a31cef9810231e39f3f85
Michael Bruce Allen <CodeAmend@Gmail.Com>
2015-09-28 12:32:04 -0400

    Rock Paper Scissors initial setup

---
+++ test/RPS.java
@@ -0,0 +1,24 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.awt.*;
+
+/**
+ * Created by codeamend on 9/28/15.
+ */
+public class RPS {
+
+    // I have three valid moves: Rock, Paper, or Scissors.
+    // The result will be either
+    // a tie when the computer and I make the same move,
+    // or I will win, or the computer will win.
+    // Scissors cut Paper, Paper covers Rock, Rock smashes Scissors.
+
+    @Test
+    public void test() {
+
+        Assert.assertTrue(false);
+
+    }
+
+}
