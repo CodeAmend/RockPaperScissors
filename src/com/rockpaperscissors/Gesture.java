c5de0b3bc83b1a3222dcd2902a48ee6c786fb17c
Michael Bruce Allen <CodeAmend@Gmail.Com>
2015-09-30 12:45:12 -0400

    added Random instantiation to the main class level

--- src/com/rockpaperscissors/Gesture.java
+++ src/com/rockpaperscissors/Gesture.java
@@ -12,6 +12,7 @@ public enum Gesture {
 
     ROCK(1, 3), PAPER(2, 1), SCISSORS(3, 2);
 
+    private static Random random = new Random();
     private int gestureNumber, losesTo;
     private static List<Gesture>
             gestureList = new ArrayList<>(Arrays.asList(Gesture.values()));
@@ -29,8 +30,7 @@ public enum Gesture {
     }
 
     public static Gesture random() {
-        Random rand = new Random();
-        int pick = rand.nextInt(Gesture.values().length);
+        int pick = random.nextInt(Gesture.values().length);
         return gestureList.get(pick);
     }
 }
