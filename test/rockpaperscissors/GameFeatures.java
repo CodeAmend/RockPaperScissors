21778ff3cf8486f45751b6be19d3f387cf28653f
Michael Bruce Allen <CodeAmend@Gmail.Com>
2015-09-30 19:46:33 -0400

    added new test class GameFeatures

---
+++ test/rockpaperscissors/GameFeatures.java
@@ -0,0 +1,15 @@
+package rockpaperscissors;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by codeamend on 9/30/15.
+ */
+public class GameFeatures extends Assert {
+
+    @Test
+    public void test() {
+        assertTrue(false);
+    }
+}
