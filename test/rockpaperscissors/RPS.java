6bbaefba5361678e5e58f47427c71e88e34a8163
Michael Bruce Allen <CodeAmend@Gmail.Com>
2015-09-30 19:40:08 -0400

    added all wins, loss, tie combinations : passed

--- test/rockpaperscissors/RPS.java
+++ test/rockpaperscissors/RPS.java
@@ -16,17 +16,46 @@ public class RPS extends Assert {
     // or I will win, or the computer will win.
     // Scissors cut Paper, Paper covers Rock, Rock smashes Scissors.
 
+    //Wins
     @Test
     public void paper_beats_rock() {
         assertTrue(PAPER.beats(ROCK));
     }
     @Test
+    public void rock_beats_scissors() {
+        assertTrue(ROCK.beats(SCISSORS));
+    }
+    @Test
+    public void scissors_beats_paper() {
+        assertTrue(SCISSORS.beats(PAPER));
+    }
+
+    //losses
+    @Test
     public void rock_loses_to_paper() {
         assertFalse(ROCK.beats(PAPER));
     }
     @Test
-    public void rock_beats_scissors() {
-        assertTrue(ROCK.beats(SCISSORS));
+    public void paper_loses_to_scissors() {
+        assertFalse(PAPER.beats(SCISSORS));
+    }
+    @Test
+    public void scissors_loses_to_rock() {
+        assertFalse(SCISSORS.beats(ROCK));
+    }
+
+    //Tie
+    @Test
+    public void rock_ties_with_rock() {
+        assertFalse(ROCK.beats(ROCK));
+    }
+    @Test
+    public void paper_ties_with_paper() {
+        assertFalse(PAPER.beats(PAPER));
+    }
+    @Test
+    public void scissors_ties_with_scissors() {
+        assertFalse(SCISSORS.beats(SCISSORS));
     }
 
 }
