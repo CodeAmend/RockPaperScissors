1b44ed4129bcea5c437c8b4d85d7089c73e93d9f
Michael Bruce Allen <CodeAmend@Gmail.Com>
2015-09-30 20:23:19 -0400

    get_random_gesture() : passed

--- src/rockpaperscissors/Gesture.java
+++ src/rockpaperscissors/Gesture.java
@@ -1,13 +1,18 @@
 package rockpaperscissors;
 
+import java.util.Arrays;
+import java.util.Random;
 /**
  * Created by codeamend on 9/30/15.
  */
+
+
 public enum Gesture {
 
     ROCK(1), PAPER(2), SCISSORS(3);
 
     private int rank;
+    static Random rand = new Random();
 
     Gesture(int rank) {
         this.rank = rank;
@@ -20,6 +25,7 @@ public enum Gesture {
     }
 
     public static Gesture getRandomGesture() {
-        return ROCK;
+        int gesturePick = rand.nextInt(Gesture.values().length);
+        return Arrays.asList(Gesture.values()).get(gesturePick);
     }
 }
